2e7527eace33ab4d6573d3917fb2713de486912f
Cristofer <cristoferjovito33@gmail.com>
2024-09-28 01:10:01 -0400

    commit de prueba 1

---
+++ main.js
@@ -0,0 +1,46 @@
+let nombre,n1,n2,n3,total;
+
+
+
+
+
+
+nombre = prompt("ingrese nombre")
+
+n1 = parseInt (prompt("ingrese primer numero"));
+
+n2 = parseInt (prompt("ingrese segundo numero"));
+
+n3 = parseInt (prompt("ingrese tercer numero"));
+
+total = n1+n2+n3
+
+
+
+
+
+
+
+if(total== 100){
+
+
+alert(`el resulado es 100`);
+
+
+
+
+}else if(total > 100){
+
+
+alert(`el resulado es mayor es ${total} por lo que es mayor a 100`);
+
+}else if(total < 100){
+
+
+
+alert(`el resulado es mayor es ${total} por lo que es menor a 100`);
+
+}
+
+
+alert(`el usuario ${nombre} ha utilizado la aplicacion`)
